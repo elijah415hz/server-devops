0836a992206c63e21e6f23681d02abce6137473c
Elijah Blaisdell <69051669+elijah415hz@users.noreply.github.com>
2023-04-01 22:34:17 -0400

    Sketch desired functionality

--- webserver.sh
+++ webserver.sh
@@ -5,3 +5,12 @@
 # Link to example webserver in bash https://gist.github.com/leandronsp/3a81e488b792235b2be73f8def2f51e6
 
 # This can be expanded to implement displaying metrics by running other commands on the host
+
+# Deploy endpoint:
+# PUT /deploy
+# Body <container-name>
+# Token $token (env variable)
+# If valid token, send <container-name> to named pipe, send response "deployment triggered for <container-name>"
+# Maybe: Set valid container names as env variables and send error response if body is invalid
+# Maybe: Sanitize input (not sure how trivial that is. Not needed if above is implemented. Removing &&, ||, ;, etc would be a first step). 
+# Maybe: Do neither of the above. Instead, just make this a messaging service that just sends to the host whatever it gets. Handle santization and parsing on the host.
